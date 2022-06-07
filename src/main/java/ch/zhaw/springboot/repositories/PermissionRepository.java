145599e41f052375aacac11cfc5b8f8d77c9b9b4
Nick Rascher <nick.rascher@hispeed.ch>
2022-06-07 11:37:20 +0200

    removed unused comment

--- src/main/java/ch/zhaw/springboot/repositories/PermissionRepository.java
+++ src/main/java/ch/zhaw/springboot/repositories/PermissionRepository.java
@@ -12,9 +12,4 @@ public interface PermissionRepository extends JpaRepository<Permission, Long> {
     @Query("SELECT p1.user AS lhs, p2.user AS rhs FROM Permission p1, Permission p2 WHERE p1.node = p2.node AND p1.user < p2.user")
     List<Permission> getEdges();
 
-    //SELECT p1.user_id AS lhs, p2.user_id AS rhs
-    //FROM permission p1, permission p2
-    //WHERE p1.node_id = p2.node_id
-    //AND p1.user_id < p2.user_id;
-
 }
